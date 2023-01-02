c6b539bee63f4dd3d8d8c2506e43a2739a11fa84
Fernanda Pereira <FernandaPereira@iheartmedia.com>
2023-01-02 17:56:14 -0300

    adding 'normal' JS logic to components

--- src/App.js
+++ src/App.js
@@ -28,19 +28,24 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem
-        date={expenses[0].date.toISOString()}
+        date={expenses[0].date}
         title={expenses[0].title}
         amount={expenses[0].amount}
       ></ExpenseItem>
       <ExpenseItem
-        date={expenses[1].date.toISOString()}
+        date={expenses[1].date}
         title={expenses[1].title}
         amount={expenses[1].amount}
       ></ExpenseItem>
       <ExpenseItem
-        date={expenses[2].date.toISOString()}
+        date={expenses[2].date}
         title={expenses[2].title}
         amount={expenses[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
       ></ExpenseItem>
     </div>
   );
