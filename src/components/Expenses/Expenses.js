80f7c0be3d2c8a926e90a4ac110e3dbc4e7196ac
Fernanda Pereira <FernandaPereira@iheartmedia.com>
2023-01-06 09:16:37 -0300

    rendering lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,26 +18,13 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          date={props.items[0].date}
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={props.items[1].date}
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={props.items[2].date}
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={props.items[3].date}
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-        ></ExpenseItem>
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
